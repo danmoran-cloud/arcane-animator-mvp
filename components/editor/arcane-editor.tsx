34293974524fdb37b7a3cc1955378d71f2f91ff7
v0agent <it+v0agent@vercel.com>
2026-06-03 14:54:36 +0000

    feat: add AuthProvider and new components to editor structure

--- components/editor/arcane-editor.tsx
+++ components/editor/arcane-editor.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { EditorProvider } from '@/lib/editor-store'
+import { AuthProvider } from '@/lib/auth-store'
 import { TopNavBar } from './top-nav-bar'
 import { AssetLibrary } from './asset-library'
 import { MapCanvas } from './map-canvas'
@@ -9,29 +10,31 @@ import { LayerTimeline } from './layer-timeline'
 
 export function ArcaneEditor() {
   return (
-    <EditorProvider>
-      <div className="h-screen flex flex-col overflow-hidden">
-        {/* Top Navigation */}
-        <TopNavBar />
-        
-        {/* Main Content */}
-        <div className="flex-1 flex overflow-hidden">
-          {/* Left Sidebar - Asset Library */}
-          <AssetLibrary />
+    <AuthProvider>
+      <EditorProvider>
+        <div className="h-screen flex flex-col overflow-hidden">
+          {/* Top Navigation */}
+          <TopNavBar />
           
-          {/* Center - Canvas and Timeline */}
-          <div className="flex-1 flex flex-col overflow-hidden">
-            {/* Map Canvas */}
-            <MapCanvas />
+          {/* Main Content */}
+          <div className="flex-1 flex overflow-hidden">
+            {/* Left Sidebar - Asset Library */}
+            <AssetLibrary />
             
-            {/* Bottom Panel - Layer Timeline */}
-            <LayerTimeline />
+            {/* Center - Canvas and Timeline */}
+            <div className="flex-1 flex flex-col overflow-hidden">
+              {/* Map Canvas */}
+              <MapCanvas />
+              
+              {/* Bottom Panel - Layer Timeline */}
+              <LayerTimeline />
+            </div>
+            
+            {/* Right Sidebar - Layer Inspector */}
+            <LayerInspector />
           </div>
-          
-          {/* Right Sidebar - Layer Inspector */}
-          <LayerInspector />
         </div>
-      </div>
-    </EditorProvider>
+      </EditorProvider>
+    </AuthProvider>
   )
 }
